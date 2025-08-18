corrected hll flux calculation

examples/mhd_tests/run_orszag_tang.py
@@ -36,7 +36,7 @@ X, Y, Z = np.meshgrid(x, y, z, indexing="ij")
 
 ############ INITIAL CONDITION #################
 
-adiabatic_idx = 5 / 3
+adiabatic_idx = 5. / 3.
 
 rho = np.ones(mesh_shape)
 
@@ -71,6 +71,7 @@ config = {
     "cfl": cfl,
     "mesh_shape": mesh_shape,
     "mesh_size": mesh_size,
+    "mesh_grid": (X, Y, Z),
     "t_max": t_max,
     "n_dumps": n_outputs,
     "initial_condition": initial_condition,

src/gawain/fluxes.py
@@ -38,7 +38,6 @@ def MHDFlux(u, axis):
     en = u.energy()
     tpressure = u.total_pressure()
     bx, by, bz = u.mag(0), u.mag(1), u.mag(2)
-    zeros = np.zeros(dens.shape)
     flux = np.array(
         [
             axis_mom,
@@ -53,7 +52,7 @@ def MHDFlux(u, axis):
         ]
     )
     flux[axis + 1] += tpressure
-    flux[axis + 5] = zeros
+    flux[axis + 5] = 0.
     return flux
 
 
@@ -77,11 +76,11 @@ class FluxCalculator:
         self.flux_function = MHDFlux if with_mhd else EulerFlux
 
     def _specific_fluxes(self, u):
-        return (
+        return [
             self.flux_function(u.get_neighbour_state(i, j), i)
             for i in range(3)
             for j in (1, -1)
-        )
+        ]
 
     def calculate_flux_divergence(self, u):
         """Flux divergence calculation"""
@@ -93,11 +92,13 @@ class FluxCalculator:
         return total_flux
 
 
+
+
 class HLLFluxer(FluxCalculator):
     """A MUSCL-Hancock HLL solver using minmod limiter
 
     This class calculates the numerical divergence of the flux
-    in the conservation equation problem using the HLLE solver
+    in the conservation equation problem using the HLL solver
     with MUSCL-hancock reconstruction and a minmod limiter.
     """
 
@@ -110,18 +111,13 @@ class HLLFluxer(FluxCalculator):
             a * b <= 0.0, 0.0, np.where(np.absolute(a) < np.absolute(b), a, b)
         )
 
+
     def wave_speeds(self, Ul, Ur, axis):
-        """Calculate the min/max wave speeds at the x-axis inteface between two cells"""
-        lambda_L_min, lambda_L_max = Ul.calculate_min_max_wave_speeds(axis)
-        lambda_R_min, lambda_R_max = Ur.calculate_min_max_wave_speeds(axis)
-
-        zeros = np.zeros(lambda_L_min.shape)
-
-        return (
-            np.minimum(np.minimum(zeros, lambda_L_min), lambda_R_min),
-            np.maximum(np.maximum(zeros, lambda_L_max), lambda_R_max),
-        )
-
+        """Calculate the min/max wave speeds at the inteface between two cells"""
+        ulminusal, ulplusal = Ul.eigen_speeds(axis)
+        urminusal, urplusal = Ur.eigen_speeds(axis)
+        return np.minimum(ulminusal, urminusal), np.maximum(ulplusal, urplusal)
+    
     def hll_flux(self, Sl, Sr, Ul, Ur, axis):
         """Calculate the HLLE flux at the interface"""
 
@@ -133,14 +129,12 @@ class HLLFluxer(FluxCalculator):
 
         fhll = (Sr * fl - Sl * fr + Sl * Sr * (ur - ul)) / (Sr - Sl)
 
-        return np.where(Sl >= 0.0, fl, np.where(Sr <= 0.0, fr, fhll))
+        return np.select([Sl >= 0.0, Sr <= 0.0, (Sl <= 0.0) & (Sr >= 0.0)], [fl, fr, fhll])
 
-    def MUSCL_Hancock_reconstruction(self, U_left, U_mid, U_right, axis):
-        """ "Calculate the MUSCL-hancock reconstruction of cell values
+    def MUSCL_Hancock_reconstruction(self, uminus ,u , uplus, axis):
+        """Calculate the MUSCL-hancock reconstruction of cell values at an cell interface"""
 
-        note: this means lefts holds the states for the left of the i+1/2 face,
-        rights holds the states for the right of the i-1/2 face.
-        """
+        
         # reconstruct
         umid = U_mid.centroid()
         a = umid - U_left.centroid()
@@ -164,35 +158,35 @@ class HLLFluxer(FluxCalculator):
         dt = U_mid.timestep
         delta = U_mid.cell_sizes[axis]
 
-        a = a + 0.5 * dt * (plus_flux - minus_flux) / delta
-        b = b + 0.5 * dt * (plus_flux - minus_flux) / delta
+        correction = 0.5 * dt * (plus_flux - minus_flux) / delta
+        a += correction
+        b += correction
 
         rights.set_centroid(a)
         lefts.set_centroid(b)
 
         return lefts, rights
 
-    def hlle_directional_flux(self, u, axis):
-        uplus = u.get_neighbour_state(axis, 1)
-        uminus = u.get_neighbour_state(axis, -1)
-        lefts, rights = self.MUSCL_Hancock_reconstruction(uminus, u, uplus, axis)
+    def hll_directional_fluxes(self, u, axis):
+        uplus1 = u.get_neighbour_state(axis, 1)
+        uminus1 = u.get_neighbour_state(axis, -1)
+        #lefts, rights = self.MUSCL_Hancock_reconstruction(uminus, u, uplus, axis)``
 
-        umid = rights
-        uminus = lefts.get_neighbour_state(0, -1)
-        Sl, Sr = self.wave_speeds(uminus, umid, 0)
-        minus_flux = self.hll_flux(Sl, Sr, uminus, umid, 0)
+        # right interface flux calculation
+        Sl, Sr = self.wave_speeds(u, uplus1, axis)
+        right_interface_flux = self.hll_flux(Sl, Sr, u, uplus1, axis)
+        
+        #left interface flux calculation
+        Sl, Sr = self.wave_speeds(u, uplus1, axis)
+        left_interface_flux = self.hll_flux(Sl, Sr, uminus1, u, axis)
 
-        umid = lefts
-        uplus = rights.get_neighbour_state(axis, 1)
-        Sl, Sr = self.wave_speeds(umid, uplus, axis)
-        plus_flux = self.hll_flux(Sl, Sr, umid, uplus, axis)
-        return plus_flux, minus_flux
+        return right_interface_flux, left_interface_flux
 
     def _specific_fluxes(self, u):
-        """HLLE solver - specifc flux calculation"""
+        """HLL solver - specifc flux calculation"""
         fluxes = []
         for i in range(3):
-            fluxes.extend(self.hlle_directional_flux(u, i))
+            fluxes.extend(self.hll_directional_fluxes(u, i))
         return fluxes
 
 
@@ -214,12 +208,12 @@ class LaxFriedrichsFluxer(FluxCalculator):
 
         fluxes = []
         for i in range(3):
-            mid_flux = self.flux_functionX(u, i)
-            u2 = u.get_neighbour_state(0, 1).centroid()
+            mid_flux = self.flux_function(u, i)
+            u2 = u.get_neighbour_state(i, 1).centroid()
             plus_flux = self.flux_function(u.get_neighbour_state(i, 1), i)
             plus_flux = 0.5 * ((plus_flux + mid_flux) - cell_sizes[i] * (u2 - u1) / dt)
             fluxes.append(plus_flux)
-            u2 = u.get_neighbour_state(0, -1).centroid()
+            u2 = u.get_neighbour_state(i, -1).centroid()
             minus_flux = self.flux_function(u.get_neighbour_state(i, -1), i)
             minus_flux = 0.5 * (
                 (minus_flux + mid_flux) - cell_sizes[i] * (u1 - u2) / dt

src/gawain/io.py
@@ -94,9 +94,7 @@ class Parameters:
         self.t_max = config["t_max"]
         self.n_outputs = config["n_dumps"]
         self.adi_idx = config["adi_idx"]
-        self.initial_condition = config["initial_condition"].astype(
-            np.float32
-        )  # make this a f32 numpy array
+        self.initial_condition = config["initial_condition"]#.astype(np.float32)  # make this a f32 numpy array
         self.source_data = config["source"] if "source" in config.keys() else None
         self.gravity_field = config["gravity"] if "gravity" in config.keys() else None
         self.boundary_type = config["boundary_type"]
@@ -138,9 +136,9 @@ class Parameters:
                 return fluxes.FluxCalculator()
             elif self.fluxer_type == "hll":
                 return fluxes.HLLFluxer()
-            elif self.fluxer == "lax-wendroff":
+            elif self.fluxer_type == "lax-wendroff":
                 return fluxes.LaxWendroffFluxer()
-            elif self.fluxer == "lax-friedrichs":
+            elif self.fluxer_type == "lax-friedrichs":
                 return fluxes.LaxFriedrichsFluxer()
         else:
             raise KeyError(

src/gawain/numerics.py
@@ -150,13 +150,16 @@ class SolutionVector:
         new_vector.timestep = self.timestep
         new_vector.boundsetter = self.boundsetter
         return new_vector
+    
+    def eigen_speeds(self,axis):
+        """Return the eigen speeds along a given axis"""
+        vel = self.vel(axis)
+        cs = self.sound_speed()
+        return vel - cs, vel + cs
 
     def calculate_min_max_wave_speeds(self, axis):
         """Return the the minimum and maximum wave speeds along a given axis"""
-        vel = self.vel(axis)
-        cs = self.sound_speed()
-        lambda1 = vel - cs
-        lambda2 = vel + cs
+        lambda1, lambda2 = self.eigen_speeds(axis)
         return np.minimum(lambda1, lambda2), np.maximum(lambda1, lambda2)
 
     def calculate_timestep(self):
@@ -276,6 +279,7 @@ class SolutionVector:
         """Return the thermal pressure field data"""
         thermal_en = self.energy() - 0.5 * self.momTotalSqr() / self.dens()
         pressure = (self.adi_idx - 1.0) * thermal_en
+        pressure = np.maximum(pressure, 0.0)
         return pressure
 
     def sound_speed(self):
@@ -354,13 +358,11 @@ class MHDSolutionVector(SolutionVector):
         quad = va2 + vs2 + np.sqrt((va2 + vs2) ** 2 - 4 * vax2 * vs2)
         return np.sqrt(0.5 * quad)
 
-    def calculate_min_max_wave_speeds(self, axis):
-        """Return the the minimum and maximum wave speeds in the specified direction"""
+    def eigen_speeds(self, axis):
         vel = self.vel(axis)
         cf = self.fast_magnetosonic_speed(axis)
-        lambda1 = vel - cf
-        lambda2 = vel + cf
-        return np.minimum(lambda1, lambda2), np.maximum(lambda1, lambda2)
+        return vel - cf, vel + cf
+
 
 class BoundarySetter:
     """Boundary value setting object